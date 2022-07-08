d48da8070eef83519682a69bc05285406e02dd46
Aldenivan Jr <aldenivan1207@gmail.com>
2022-07-08 04:04:20 -0300

    fix: fixing the file app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,14 @@ import { routerUser } from "./routes";
 
 const app = express();
 const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
 app.use(json());
 app.use("/api", routerUser);
-app.use(cors());
+app.use(cors(corsOptions));
 
 export default app;
