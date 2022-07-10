cb4ae3dff718c3e0580b558c11100ae58eb9fdca
Aldenivan Jr <aldenivan1207@gmail.com>
2022-07-10 11:30:36 -0300

    fiX: fixing config cors

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,8 @@ import { routerUser } from "./routes";
 const app = express();
 const cors = require("cors");
 
-app.use(cors());
 app.use(json());
+app.use(cors());
 app.use("/api", routerUser);
 
 export default app;
